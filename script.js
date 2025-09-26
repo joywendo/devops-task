474b7d86851f80572bf3f9524351af9df4aadbb4
joywendo <wendokingoo@gmail.com>
2025-09-26 15:32:21 +0300

    css and js files modified

---
+++ script.js
@@ -0,0 +1,18 @@
+const text = "Welcome to my simple app!";
+const typewriter = document.getElementById("typewriter");
+
+let i = 0;
+function typeEffect() {
+  if (i < text.length) {
+    typewriter.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeEffect, 100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", typeEffect);
+
+
+
+console.log("JS is working!");
+
