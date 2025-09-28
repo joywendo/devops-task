eae167477e5262691a7593b2ca9e858195ba60fd
joywendo <wendokingoo@gmail.com>
2025-09-28 23:22:48 +0300

    Added output to show containerIP and port after setup

---
+++ terraform/outputs.tf
@@ -0,0 +1,18 @@
+output "vm_static_ip" {
+  description = "Static IP assigned on the Docker network"
+  value       = docker_container.vm.networks_advanced[0].ipv4_address
+}
+
+output "access_urls" {
+  description = "Host URLs to access the VM web service (HTTP/HTTPS)"
+  value = {
+    http  = "http://localhost:${var.host_http_port}"
+    https = "https://localhost:${var.host_https_port}"
+  }
+}
+
+output "container_id" {
+  description = "Docker container ID"
+  value       = docker_container.vm.id
+}
+
